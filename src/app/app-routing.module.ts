ee9cc2e9ded38a3658484cf93f4b728f0f8aa94f
Zendelle Badiang <Zendelle@udel.edu>
2020-05-28 20:59:34 -0400

    Content added to HomeComponent. Slight styling added

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,11 +1,11 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { MenuComponent } from 'src/components/menu/menu.component';
+import { HomeComponent } from 'src/components/home/home.component';
 
 
 const routes: Routes = [
-  
-];
+  { path: 'home', component: HomeComponent }];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,11 +4,13 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { MenuComponent } from '../components/menu/menu.component';
+import { HomeComponent } from '../components/home/home.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    MenuComponent
+    MenuComponent,
+    HomeComponent
   ],
   imports: [
     BrowserModule,
